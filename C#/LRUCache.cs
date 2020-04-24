34d4c2d204517434b86abcaaf42b3ebeae86a457
Subham Raj <subhamraj4114@gmail.com>
2020-04-25 03:07:05 +0530

    LRUCache added

---
+++ C#/LRUCache.cs
@@ -0,0 +1,66 @@
+using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+public class LRUCache {
+    int size;
+    LinkedList<Tuple<int, int>> lruCache;
+    Dictionary<int, LinkedListNode<Tuple<int, int>>> map;
+    public LRUCache (int capacity) {
+        size = capacity;
+        lruCache = new LinkedList<Tuple<int, int>> ();
+        map = new Dictionary<int, LinkedListNode<Tuple<int, int>>> ();
+    }
+
+    public int Get (int key) {
+        if (map.ContainsKey (key)) {
+            LinkedListNode<Tuple<int, int>> temp = map[key];
+            lruCache.Remove (temp);
+            AddLast (temp.Value.Item1, temp.Value.Item2);
+            return temp.Value.Item2;
+        }
+        return -1;
+    }
+
+    public void Put (int key, int value) {
+        if (!map.ContainsKey (key)) {
+            if (lruCache.Count == size) {
+                LinkedListNode<Tuple<int, int>> temp = lruCache.First;
+                map.Remove (temp.Value.Item1);
+                lruCache.Remove (temp);
+            }
+            AddLast (key, value);
+        } else {
+            lruCache.Remove (map[key]);
+            AddLast (key, value);
+        }
+    }
+
+    public void AddLast (int key, int value) {
+        LinkedListNode<Tuple<int, int>> node = new LinkedListNode<Tuple<int, int>> (
+            new Tuple<int, int> (key, value));
+        lruCache.AddLast (node);
+        map[key] = node;
+    }
+}
+
+class DriverLRU {
+    public static void Main (String[] args) {
+        LRUCache obj = new LRUCache (2);
+        obj.Put (2, 1);
+        obj.Put (1, 1);
+        obj.Put (2, 3);
+        obj.Put (4, 1);
+        Console.WriteLine (obj.Get (1));
+        Console.WriteLine (obj.Get (2));
+    }
+}
--- C#/TestNewStuffs.cs
+++ C#/TestNewStuffs.cs
@@ -1,6 +1,7 @@
+using System;
 using System.CodeDom.Compiler;
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
@@ -8,36 +9,49 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
 using System.Text;
-using System;
+using System.Text.RegularExpressions;
 class TNS {
-    static void Print<T>(T a){
-        Console.WriteLine(a);
+    static void Print<T> (T a) {
+        Console.WriteLine (a);
     }
-    // static void Main(string[] args) {
-    //     int a = 3; //0011
-    //     int b = 4; //0100
+    // static void Main (string[] args) {
+    // int a = 3; //0011
+    // int b = 4; //0100
 
-    //     Console.WriteLine(a | b); // 7
-    //     Console.WriteLine(a & b); // 0
-    //     Console.WriteLine(a ^ b); // 7
-    //     Console.WriteLine(~a); // -4
-    //     Console.WriteLine(~b); // -5
-    //     Console.WriteLine(-123 ^ -123); //0
-    //     Console.WriteLine(a >> 1); // 1
-    //     Console.WriteLine(a << 1); // 6
-    //     Console.WriteLine(b >> 1); // 2
-    //     Console.WriteLine(b << 1); // 8
-    //     Console.WriteLine("~~~~~~~~~~~~~~~~~~");
-    //     Console.WriteLine('z' - 'a');
-    //     Console.WriteLine("~~~~~~~~~~~~~~~~~~");
-    //     double temp = 0;
-    //     for(int i = 31;i >= 0; i--){
-    //         temp += Math.Pow(2,i);
-    //         Console.Write(temp+" ");
-    //     }
-    //     Console.WriteLine();
-    //     Console.Write(Convert.ToString((uint)Math.Pow(2,31) + (uint)Math.Pow(2,30),2));
+    // Console.WriteLine(a | b); // 7
+    // Console.WriteLine(a & b); // 0
+    // Console.WriteLine(a ^ b); // 7
+    // Console.WriteLine(~a); // -4
+    // Console.WriteLine(~b); // -5
+    // Console.WriteLine(-123 ^ -123); //0
+    // Console.WriteLine(a >> 1); // 1
+    // Console.WriteLine(a << 1); // 6
+    // Console.WriteLine(b >> 1); // 2
+    // Console.WriteLine(b << 1); // 8
+    // Console.WriteLine("~~~~~~~~~~~~~~~~~~");
+    // Console.WriteLine('z' - 'a');
+    // Console.WriteLine("~~~~~~~~~~~~~~~~~~");
+    // double temp = 0;
+    // for (int i = 31; i >= 0; i--) {
+    //     temp += Math.Pow (2, i);
+    //     Console.Write (temp + " ");
+    // }
+    // for (int i = 0; i < 33; i++) {
+    //     temp += Math.Pow (2, i);
+    //     Console.Write (temp + " ");
+    // }
+    // Console.WriteLine ();
+    // Console.WriteLine (temp);
+    // Console.Write(Convert.ToString((uint)Math.Pow(2,31) + (uint)Math.Pow(2,30),2));
+
+    // Dictionary<int, LinkedListNode<Tuple<int, int>>> map = new Dictionary<int, LinkedListNode<Tuple<int, int>>> ();
+    // LinkedList<Tuple<int, int>> testList = new LinkedList<Tuple<int, int>> ();
+    // LinkedListNode<Tuple<int, int>> temp = new LinkedListNode<Tuple<int, int>> (new Tuple<int, int> (1, 2));
+    // map.Add (1, temp);
+    // testList.AddLast (temp);
+    // testList.AddLast (new Tuple<int, int> (5, 3));
+    // Console.WriteLine (map[1].Next.Value);
+    // Console.WriteLine (testList.First.Value);
     // }
 }
